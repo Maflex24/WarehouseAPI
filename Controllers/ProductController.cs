34d765f728935eac76596dc35f94396ad2757b19
Gabriel Szałajko <maflex24@gmail.com>
2022-04-09 00:15:39 +0200

    Added service to GetAllProducts Query

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,15 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseAPI.Entities;
+using WarehouseAPI.Models;
+using WarehouseAPI.Services;
 
 namespace WarehouseAPI.Controllers
 {
     [Route("api/product")]
     public class ProductController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult GetAll()
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService ProductService)
         {
-            // todo
-            return StatusCode(501);
+            _productService = ProductService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ProductModelDto>> GetAll([FromQuery] int resultsAmount, [FromQuery] bool isWarehouseQuery)
+        {
+            var products = _productService.GetProducts(resultsAmount, isWarehouseQuery);
+
+            if (products == null)
+                return NotFound();
+
+            return Ok(products);
         }
     }
 }
---
+++ Services/ProductService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseAPI.Entities;
+using WarehouseAPI.Models;
+
+namespace WarehouseAPI.Services
+{
+    public interface IProductService
+    {
+        public List<ProductModelDto> GetProducts(int resultsAmount, bool isWarehouseQuery);
+    }
+
+    public class ProductService : IProductService
+    {
+        private readonly WarehouseDbContext _dbContext;
+
+        public ProductService(WarehouseDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public List<ProductModelDto> GetProducts(int resultsAmount, bool isWarehouseQuery)
+        {
+            if (resultsAmount < 1)
+                resultsAmount = 1;
+
+            var products = _dbContext
+                .Products
+                .Include(p => p.Sex)
+                .Include(p => p.Color)
+                .Include(p => p.Size)
+                .Include(p => p.Category)
+                .Take(resultsAmount)
+                .ToList();
+
+            var clientDtos = new List<ProductModelDto>();
+            foreach (var product in products)
+            {
+                clientDtos.Add(new ProductModelDto()
+                {
+                    Name = product.Name,
+                    Category = product.Category.Name,
+                    Size = product.Size.Name,
+                    Color = product.Color.Name,
+                    Sex = product.Sex.Name,
+                    Code = GetProductCode(product.Code, isWarehouseQuery) // is null if shouldn't be public
+                });
+            }
+
+            return clientDtos;
+        }
+
+        private string? GetProductCode(string productCode, bool isWarehouseQuery)
+        {
+            if (isWarehouseQuery)
+                return productCode;
+
+            return null;
+        }
+    }
+
+}
