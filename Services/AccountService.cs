b8c69342feba22100d7e8df3928eb17e84abab7c
Gabriel Szałajko <maflex24@gmail.com>
2022-04-09 16:28:55 +0200

    Added EmployeeRegister Added checking is client or employee exist, before add new the same to database

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -7,22 +7,32 @@ namespace WarehouseAPI.Services
     public interface IAccountService
     {
         public void RegisterClient(RegisterClientModelDto clientModel);
+        public void RegisterEmployee(RegisterEmployeeModelDto employeeModel);
     }
 
     public class AccountService : IAccountService
     {
         private readonly WarehouseDbContext _dbContext;
         private readonly IPasswordHasher<Client> _clientPasswordHasher;
+        private readonly IPasswordHasher<Employee> _employeePasswordHasher;
 
-        public AccountService(WarehouseDbContext dbContext, IPasswordHasher<Client> clientPasswordHasher)
+        public AccountService(WarehouseDbContext dbContext, IPasswordHasher<Client> clientPasswordHasher, IPasswordHasher<Employee> employeePasswordHasher)
         {
             _dbContext = dbContext;
             _clientPasswordHasher = clientPasswordHasher;
+            _employeePasswordHasher = employeePasswordHasher;
         }
 
 
         public void RegisterClient(RegisterClientModelDto clientModel)
         {
+            bool isMailInDatabase = _dbContext
+                .Clients
+                .Any(c => c.Email == clientModel.Email);
+
+            if (isMailInDatabase)
+                throw new Exception("This user already exist");
+
             var address = new Address()
             {
                 HouseNumber = clientModel.HouseNumber,
@@ -52,6 +62,29 @@ namespace WarehouseAPI.Services
                 .FirstOrDefault();
 
             _dbContext.Clients.Add(client);
+            _dbContext.SaveChanges();
+        }
+
+        public void RegisterEmployee(RegisterEmployeeModelDto employeeModel)
+        {
+            if (_dbContext.Employees.Any(e => e.Login == employeeModel.Login
+                                              || _dbContext.Employees.Any(e =>
+                                                  e.BadgeNumber == employeeModel.BadgeNumber)))
+                throw new Exception("Employee with this login or badge number already exist");
+
+
+            var employee = new Employee()
+            {
+                BadgeNumber = employeeModel.BadgeNumber,
+                FirstName = employeeModel.FirstName,
+                LastName = employeeModel.LastName,
+                Login = employeeModel.Login,
+                PhoneNumber = employeeModel.PhoneNumber
+            };
+
+            employee.PasswordHash = _employeePasswordHasher.HashPassword(employee, employeeModel.Password);
+
+            _dbContext.Employees.Add(employee);
             _dbContext.SaveChanges();
         }
     }
